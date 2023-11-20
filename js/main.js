ce933a570e20bc8e8866a19b1441f2692c863ccd
MikuX-Dev <abinishanth3464@gmail.com>
2023-11-20 18:53:57 +0530

    The diff changes the weather API from OpenWeatherMap to AccuWeather.

--- js/main.js
+++ js/main.js
@@ -27,40 +27,28 @@ function dateTime() {
 // }
 
 
-function getWeatherByCoordinates(latitude, longitude) {
-	const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}`;
-	
-	fetch(apiUrl)
-			.then(function (resp) {
+function weatherBalloon(cityID) {
+	fetch('https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?q=' + cityID + '&apikey=YOUR_API_KEY')
+		.then(function(resp) {
+			return resp.json();
+		})
+		.then(function(locationData) {
+			let locationKey = locationData.Key;
+			fetch('https://dataservice.accuweather.com/currentconditions/v1/' + locationKey + '?apikey=YOUR_API_KEY')
+				.then(function(resp) {
 					return resp.json();
-			})
-			.then(function (data) {
-					let weatherIcon = data.weather[0].icon;
-					let tempK = parseFloat(data.main.temp);
-					let tempC = Math.round(tempK - 273.15);
-					let tempF = Math.round((tempK - 273.15) * 1.8) + 32;
-					document.getElementById('weather').innerHTML = '<p id="location">' + data.name + '</p><p id="details" ' + 'title="' + tempC + '&deg;C">' + '<img src="https://openweathermap.org/img/wn/' + weatherIcon + '.png">' + data.weather[0].description + '<span class="separator">|</span>' + tempC + '&deg;C</p>';
-			})
-			.catch(function (error) {
-					console.error('Error fetching weather:', error);
-			});
+				})
+				.then(function(weatherData) {
+					let weatherIcon = weatherData[0].WeatherIcon;
+					let tempC = weatherData[0].Temperature.Metric.Value;
+					let tempF = weatherData[0].Temperature.Imperial.Value;
+					document.getElementById('weather').innerHTML = '<p id="location">' + locationData.EnglishName + '</p><p id="details" ' + 'title="' + tempC + '&deg;C">' + '<img src="https://www.accuweather.com/images/weathericons/' + weatherIcon + '.svg">' + weatherData[0].WeatherText + '<span class="separator">|</span>' + tempC + '&deg;C</p>';
+				});
+		});
 }
 
 function traichu() {
 	dateTime();
-
-	// Use navigator.geolocation to get user's coordinates
-	if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(function (position) {
-					const latitude = position.coords.latitude;
-					const longitude = position.coords.longitude;
-
-					// Call the weather function with the coordinates
-					getWeatherByCoordinates(latitude, longitude);
-			}, function (error) {
-					console.error('Error getting location:', error);
-			});
-	} else {
-			console.error('Geolocation is not supported by this browser.');
-	}
+	// Use the latitude and longitude of the desired location instead of city ID
+	weatherBalloon('latitude,longitude');
 }
