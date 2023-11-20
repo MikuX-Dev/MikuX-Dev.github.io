d735c8b9e13b2a077748fd5044475869453fb25a
MikuX-Dev <abinishanth3464@gmail.com>
2023-11-20 18:32:12 +0530

    ``` feat: Detect user location and display weather
    This commit adds a new function called weatherBalloonAutoDetect,
    which uses the user's geolocation to fetch weather data and
    display it on the page.
    ```

--- js/main.js
+++ js/main.js
@@ -6,22 +6,37 @@ function dateTime() {
 	setTimeout(dateTime, 1000);
 }
 
-function weatherBalloon(cityID) {
-	var apiKey = '912416a3726142366d9456d54cfd594b'; //OpenWeather API key
-	fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + apiKey)
-		.then(function(resp) {
-			return resp.json()
-		})
-		.then(function(data) {
-			let weatherIcon = data.weather[0].icon;
-			let tempK = parseFloat(data.main.temp);
-			let tempC = Math.round(tempK - 273.15);
-			let tempF = Math.round((tempK - 273.15) * 1.8) + 32;
-			document.getElementById('weather').innerHTML = '<p id="location">' + data.name + '</p><p id="details" ' + 'title="' + tempC + '&deg;C">' + '<img src="https://openweathermap.org/img/wn/' + weatherIcon + '.png">' + data.weather[0].description + '<span class="separator">|</span>' + tempC + '&deg;C</p>';
-		});
+function weatherBalloonAutoDetect() {
+	if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function(position) {
+					// Get latitude and longitude from the user's position
+					const latitude = position.coords.latitude;
+					const longitude = position.coords.longitude;
+					// Fetch weather data using the coordinates
+					fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=912416a3726142366d9456d54cfd594b`)
+							.then(function(resp) {
+									return resp.json();
+							})
+							.then(function(data) {
+									let weatherIcon = data.weather[0].icon;
+									let tempK = parseFloat(data.main.temp);
+									let tempC = Math.round(tempK - 273.15);
+									let tempF = Math.round((tempK - 273.15) * 1.8) + 32;
+									document.getElementById('weather').innerHTML = '<p id="location">' + data.name + '</p><p id="details" ' + 'title="' + tempC + '&deg;C">' + '<img src="https://openweathermap.org/img/wn/' + weatherIcon + '.png">' + data.weather[0].description + '<span class="separator">|</span>' + tempC + '&deg;C</p>';
+							})
+							.catch(function(error) {
+									console.error('Error fetching weather data:', error);
+							});
+			}, function(error) {
+					console.error('Error getting user location:', error);
+			});
+	} else {
+			console.error('Geolocation is not supported by this browser.');
+	}
 }
 
 function traichu() {
 	dateTime();
-	weatherBalloon(1273865); //OpenWeather city ID
+	weatherBalloonAutoDetect();
 }
+
