82425a59dfa93c11d9e162f981510a4fdf9b3759
MikuX-Dev <abinishanth3464@gmail.com>
2023-11-20 18:46:46 +0530

    The diff adds a new function called `getWeatherByCoordinates` that takes latitude and longitude as parameters and fetches weather data from the OpenWeatherMap API.
    The function is then called in the `traichu` function, which is responsible for getting the user's location and displaying the weather.
    
    The diff also removes the old `weatherBalloonAutoDetect` function, which was used to fetch weather data for a specific city ID.

--- js/main.js
+++ js/main.js
@@ -6,37 +6,61 @@ function dateTime() {
 	setTimeout(dateTime, 1000);
 }
 
-function weatherBalloonAutoDetect() {
+// function weatherBalloon(cityID) {
+// 	var apiKey = '912416a3726142366d9456d54cfd594b'; //OpenWeather API key
+// 	fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + apiKey)
+// 		.then(function(resp) {
+// 			return resp.json()
+// 		})
+// 		.then(function(data) {
+// 			let weatherIcon = data.weather[0].icon;
+// 			let tempK = parseFloat(data.main.temp);
+// 			let tempC = Math.round(tempK - 273.15);
+// 			let tempF = Math.round((tempK - 273.15) * 1.8) + 32;
+// 			document.getElementById('weather').innerHTML = '<p id="location">' + data.name + '</p><p id="details" ' + 'title="' + tempC + '&deg;C">' + '<img src="https://openweathermap.org/img/wn/' + weatherIcon + '.png">' + data.weather[0].description + '<span class="separator">|</span>' + tempC + '&deg;C</p>';
+// 		});
+// }
+
+// function traichu() {
+// 	dateTime();
+// 	weatherBalloon(1273865); //OpenWeather city ID
+// }
+
+
+function getWeatherByCoordinates(latitude, longitude) {
+	const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}`;
+	
+	fetch(apiUrl)
+			.then(function (resp) {
+					return resp.json();
+			})
+			.then(function (data) {
+					let weatherIcon = data.weather[0].icon;
+					let tempK = parseFloat(data.main.temp);
+					let tempC = Math.round(tempK - 273.15);
+					let tempF = Math.round((tempK - 273.15) * 1.8) + 32;
+					document.getElementById('weather').innerHTML = '<p id="location">' + data.name + '</p><p id="details" ' + 'title="' + tempC + '&deg;C">' + '<img src="https://openweathermap.org/img/wn/' + weatherIcon + '.png">' + data.weather[0].description + '<span class="separator">|</span>' + tempC + '&deg;C</p>';
+			})
+			.catch(function (error) {
+					console.error('Error fetching weather:', error);
+			});
+}
+
+function traichu() {
+	dateTime();
+
+	// Use navigator.geolocation to get user's coordinates
 	if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(function(position) {
-					// Get latitude and longitude from the user's position
+			navigator.geolocation.getCurrentPosition(function (position) {
 					const latitude = position.coords.latitude;
 					const longitude = position.coords.longitude;
-					// Fetch weather data using the coordinates
-					fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=912416a3726142366d9456d54cfd594b`)
-							.then(function(resp) {
-									return resp.json();
-							})
-							.then(function(data) {
-									let weatherIcon = data.weather[0].icon;
-									let tempK = parseFloat(data.main.temp);
-									let tempC = Math.round(tempK - 273.15);
-									let tempF = Math.round((tempK - 273.15) * 1.8) + 32;
-									document.getElementById('weather').innerHTML = '<p id="location">' + data.name + '</p><p id="details" ' + 'title="' + tempC + '&deg;C">' + '<img src="https://openweathermap.org/img/wn/' + weatherIcon + '.png">' + data.weather[0].description + '<span class="separator">|</span>' + tempC + '&deg;C</p>';
-							})
-							.catch(function(error) {
-									console.error('Error fetching weather data:', error);
-							});
-			}, function(error) {
-					console.error('Error getting user location:', error);
+
+					// Call the weather function with the coordinates
+					getWeatherByCoordinates(latitude, longitude);
+			}, function (error) {
+					console.error('Error getting location:', error);
 			});
 	} else {
 			console.error('Geolocation is not supported by this browser.');
 	}
 }
-
-function traichu() {
-	dateTime();
-	weatherBalloonAutoDetect();
-}
-
